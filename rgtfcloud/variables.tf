eb5c65984bc9768d6878a31d8e288cba66a97b0b
Kunlesanni <42106750+Kunlesanni@users.noreply.github.com>
2022-05-10 17:09:34 +0100

    Add files via upload

---
+++ rgtfcloud/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  default       = "test-rg01"
+  description   = "Resource group name where the new resources to be created."
+}
+
+variable "location" {
+  default       = "westeurope"
+  description   = "Location of the VM"
+}
