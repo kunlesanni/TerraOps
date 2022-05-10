a510166c669ae0d65d76d12f00d08cb2f6f74cde
Kunlesanni <42106750+Kunlesanni@users.noreply.github.com>
2022-05-10 17:12:33 +0100

    Update providers.tf

--- rgtfcloud/providers.tf
+++ rgtfcloud/providers.tf
@@ -12,8 +12,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "a564e508-b2a0-480e-9729-97d54d09f44e"
-  client_id       = "b6fc9423-3b96-418c-8a66-30e673cd6878"
-  client_secret   = "ydj8Q~5f2l2jK6TSYyNji4mkudB~jpfsFWlp2akA"
-  tenant_id       = "251fe175-13cb-400c-affc-7105f5c904e2"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 }
