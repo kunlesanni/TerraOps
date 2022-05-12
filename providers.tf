b8efa1929b0f7fcba51b4b4bb14b0ee86443ce40
Kunlesanni <42106750+Kunlesanni@users.noreply.github.com>
2022-05-12 09:56:01 +0100

    Added WindowsVM Files

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+}
