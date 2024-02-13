1743c51b45b73ff7aa4dcde80eb34852db52feac
osanni <42106750+kunlesanni@users.noreply.github.com>
2024-02-13 08:29:02 +0000

    Update main.tf

--- private_dns_zone/main.tf
+++ private_dns_zone/main.tf
@@ -38,22 +38,4 @@ resource "azurerm_private_dns_zone_virtual_network_link" "pdns_zone" {
   virtual_network_id    = var.vnet_id
 }
 
-variable "rg" {
-    description = "The name of the resource group where the private dns zone will be created"
-}
 
-variable "pdns_zone" {
-    description = "The name of the private dns zone"
-}
-
-variable "tags" {
-    description = "The tags to be assigned to the private dns zone"
-}
-
-variable "vnet" {
-    description = "The name of the virtual network"
-}
-
-variable "vnet_id" {
-    description = "The name of the virtual network"
-}
